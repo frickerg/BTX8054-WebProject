6b880e4bdfbeb4b68398da2e9b89b04753403862
Guillaume Fricker <fricker.g@zoho.com>
2018-05-12 13:11:02 +0200

    Updated listPatients.php (upgrade to Bootstrap 4)
    //TODO : patientList layout must be upgraded to bootstrap 4.0

--- src/listPatients.php
+++ src/listPatients.php
@@ -14,17 +14,26 @@ include 'pdo.inc.php';
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     /*** echo a message saying we have connected ***/
+    echo '<div class="container">';
     echo '<h1>List of patients</h1>';
     $sql = 'select * from patient';
 
     $result = $dbh->query($sql);
-
+    echo '<div class="row">';
     while ($line = $result->fetch()) {
-        echo "<a href='viewPatient.php?id=".$line['patientID']."'>";
-        echo $line['first_name'].' '.$line['name'];
-
-        echo "</a><br>\n";
+        echo '<div class="col-xs-12 col-sm-6 col-lg-4">';
+        echo '<div class="card">';
+        echo '<div class="card-img-top img-responsive" style="background: url(img/patient/'.strtolower($line['first_name'].$line['name']).'.jpg) no-repeat center center"></div>';
+        echo '<div class="card-body">';
+        echo '<h2 class="card-title">'.$line['first_name'].' '.$line['name'].'</h2>';
+        echo '<p class="card-text">lorem ipsum</p>';
+        echo '<a class="btn btn-primary" href="viewPatient.php?id='.$line['patientID'].'">View Patient</a>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
     }
+    echo '</div>';
+    echo '</div>';
 
     $dbh = null;
 } catch (PDOException $e) {
@@ -36,5 +45,6 @@ echo '<br>User ='.$_SESSION['user'];
 ?>
 <br />
 <i><a href="logout.php">Logout</a></i>
+
 
 <?php include 'template/footer.php'; ?>
