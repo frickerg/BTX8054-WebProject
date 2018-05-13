a1b1346e0db82026cd18a1c3d288b6a5be448197
Guillaume Fricker <fricker.g@zoho.com>
2018-05-13 17:40:48 +0200

    Created new footer template

--- src/template/footer.php
+++ src/template/footer.php
@@ -1,2 +1,21 @@
+<footer>
+    <div class="container">
+		<div class="row" id="footerLinks">
+			<div id="logoBfh" class="col-sm-4 d-none d-sm-flex">
+				<img id="logoFooter" class="img-responsive" src="img/bfh.png" alt="BFH - Bern University Of Applied Sciences">
+			</div>
+			<div class="col-12 col-sm-8">
+		        <ul>
+		            <li><a href="https://www.ti.bfh.ch/en/bachelor/medical_informatics.html">BFH - Medizininformatik</a></li>
+		            <li><a href="https://www.benoist.ch/WebApps/exercises/recap.php"><i class="fab far fa-edit"></i>&nbsp;Project description</a></li>
+					<li><a href="https://github.com/frickerg/BTX8054-WebProject"><i class="fab fa-github"></i>&nbsp;GitHub</a></li>
+		        </ul>
+			</div>
+			<div class="col-12 col-sm-8 offset-sm-4">
+				<div class="footer-copyright text-muted">&copy; 2018, Guillaume Fricker</div>
+			</div>
+		</div>
+    </div>
+</footer>
 </body>
 </html>
