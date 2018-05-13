1155b09a4a134b9d633b17405a083381fab0efb3
Guillaume Fricker <fricker.g@zoho.com>
2018-05-13 18:41:09 +0200

    Added jQuery Bootstrap 4 Plugins

--- src/template/footer.php
+++ src/template/footer.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="container">
 		<div class="row" id="footerLinks">
-			<div id="logoBfh" class="col-sm-4 d-none d-sm-flex">
+			<div id="logoBfh" class="col-sm-6 col-md-4 d-none d-sm-flex">
 				<img id="logoFooter" class="img-responsive" src="img/bfh.png" alt="BFH - Bern University Of Applied Sciences">
 			</div>
-			<div class="col-12 col-sm-8">
+			<div class="col-12 col-sm-6 col-md-8">
 		        <ul>
 		            <li><a href="https://www.ti.bfh.ch/en/bachelor/medical_informatics.html">BFH - Medizininformatik</a></li>
 		            <li><a href="https://www.benoist.ch/WebApps/exercises/recap.php"><i class="fab far fa-edit"></i>&nbsp;Project description</a></li>
